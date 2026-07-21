51b0e216537b997f7b73e0e56b62e0420f88c849
Arun93-TB <legendarunkumar47@gmail.com>
2026-07-21 12:37:36 +0530

    alter

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function analyze() {
+
     const data = {
         sleep: document.getElementById("sleep").value,
         work: document.getElementById("work").value,
@@ -9,16 +10,40 @@ function analyze() {
 
     fetch("http://127.0.0.1:5000/analyze", {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify(data)
     })
-    .then(res => res.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Server Error");
+        }
+        return response.json();
+    })
     .then(result => {
+
         document.getElementById("result").innerHTML = `
-            <h3>Risk Score: ${result.risk_score}</h3>
-            <h3>Risk Level: ${result.risk_level}</h3>
-            <p><b>Causes:</b> ${result.causes.join(", ")}</p>
-            <p><b>My Advice:</b><br> ${result.actions.join("<br>")}</p>
+            <h3>Risk Score : ${result.risk_score}</h3>
+            <h3>Risk Level : ${result.risk_level}</h3>
+            <h3>Health Score : ${result.health_score}</h3>
+
+            <p><b>Causes:</b><br>${result.causes.join("<br>")}</p>
+
+            <p><b>Advice:</b><br>${result.actions.join("<br>")}</p>
+
+            <p><b>Motivation:</b><br>${result.motivation}</p>
+
+            <p><b>Wellness Tip:</b><br>${result.wellness_tip}</p>
+
+            <p><b>Time:</b> ${result.analysis_time}</p>
         `;
+
+    })
+    .catch(error => {
+        console.log(error);
+        document.getElementById("result").innerHTML =
+            "<h2 style='color:red;'>Unable to connect to Flask Backend</h2>";
     });
+
 }
